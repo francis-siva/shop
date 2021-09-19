60e81fc95bd42a90de6ae2c6009cdc5bd1d013e4
francis-siva <fsivasithamparam@gmail.com>
2021-09-19 22:54:50 +0200

    Connexion à PostgreSQL + Configuration de JPA & Hibernate

--- src/main/java/com/codestudio/corp/shop/App.java
+++ src/main/java/com/codestudio/corp/shop/App.java
@@ -2,15 +2,16 @@ package com.codestudio.corp.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EntityScan("com.codestudio.corp.model")
 @RestController
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main(String[] args) {
     	SpringApplication.run(App.class, args);
     }
     
